331d07513c4cf6916b20f4dd4ed69a3ec4767fb9
Admin <congloc@vutatech.vn>
2021-04-17 19:00:47 +0700

    orders1704

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-
- <table border="1" v-if="customerGroups != null">
+  <div>
+    <h2>CustomerLists by Orders</h2>
+    <table border="1" v-if="customerGroups != null">
       <tr>
         <td>ID</td>
         <td>NAME</td>
+        <td>email</td>
+        <td>phone</td>
+        <td>customer_group_id</td>
+        <td>address_1</td>
+        <td>province_id</td>
+        <td>ward_id</td>
+        <td>address_2</td>
+        <td>facebook_link</td>
+        <td>birthday</td>
+        <td>customer_state_id</td>
+        <td>user_id</td>
+        <td>text</td>
+        <td>price_policy_id</td>
+        <td>tax</td>
+        <td>payment_id</td>
+        <td>after_sales_service_id</td>
         <td>Created_at</td>
         <td>Updated_at</td>
       </tr>
@@ -12,7 +29,55 @@
           {{ customerGroup.id }}
         </td>
         <td>
-          {{ customerGroup.name }}
+          {{ customerGroup.full_name }}
+        </td>
+        <td>
+          {{ customerGroup.email }}
+        </td>
+        <td>
+          {{ customerGroup.phone }}
+        </td>
+        <td>
+          {{ customerGroup.customer_group_id }}
+        </td>
+        <td>
+          {{ customerGroup.address_1 }}
+        </td>
+        <td>
+          {{ customerGroup.province_id }}
+        </td>
+        <td>
+          {{ customerGroup.ward_id }}
+        </td>
+        <td>
+          {{ customerGroup.address_2 }}
+        </td>
+        <td>
+          {{ customerGroup.facebook_link }}
+        </td>
+        <td>
+          {{ customerGroup.birthday }}
+        </td>
+        <td>
+          {{ customerGroup.customer_state_id }}
+        </td>
+        <td>
+          {{ customerGroup.user_id }}
+        </td>
+        <td>
+          {{ customerGroup.text }}
+        </td>
+        <td>
+          {{ customerGroup.price_policy_id }}
+        </td>
+        <td>
+          {{ customerGroup.tax }}
+        </td>
+        <td>
+          {{ customerGroup.payment_id }}
+        </td>
+        <td>
+          {{ customerGroup.after_sales_service_id }}
         </td>
         <td>
           {{ customerGroup.created_at }}
@@ -25,29 +90,54 @@
     <div v-else>
       <p>kiem tra lai</p>
     </div>
-
-
+  </div>
 </template>
 <script>
 import axios from "axios";
 export default {
-  data(){
-    return{
-      customerGroups : [],
-      name :'',
-    }
+  props: {
+    // tong so trang
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    // so thu tu cua trang hien tai
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    // so record trong 1 trang
+    item: {
+      type: Number,
+      required: true,
+    },
+  },
+  state: {
+    currentPage: 1,
+  },
+  data() {
+    return {
+      customerGroups: [],
+      full_name: "",
+    };
   },
   created() {
     axios
-      .get("http://127.0.0.1:8000/customer-groups?limit=:limit")
+      .get("http://127.0.0.1:8000/customers?limit=:limit")
       .then((response) => {
-        console.log(response)
+        console.log(response);
         this.customerGroups = response.data;
       })
       .catch((e) => {
         console.log(e);
       });
   },
-  
-}
+  // method: {
+  //   pagination: {
+  //     totalPages = this.customerGroups.count/this.item
+  //     currentPage = count
+  //   }
+  // }
+  methods: {},
+};
 </script>
---
+++ src/order17.vue
@@ -0,0 +1,53 @@
+<template>
+
+ <table border="1" v-if="customerGroups != null">
+      <tr>
+        <td>ID</td>
+        <td>NAME</td>
+        <td>Created_at</td>
+        <td>Updated_at</td>
+      </tr>
+      <tr v-for="customerGroup in customerGroups.data" :key="customerGroup.id">
+        <td>
+          {{ customerGroup.id }}
+        </td>
+        <td>
+          {{ customerGroup.name }}
+        </td>
+        <td>
+          {{ customerGroup.created_at }}
+        </td>
+        <td>
+          {{ customerGroup.updated_at }}
+        </td>
+      </tr>
+    </table>
+    <div v-else>
+      <p>kiem tra lai</p>
+    </div>
+
+
+</template>
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      customerGroups : [],
+      name :'',
+    }
+  },
+  created() {
+    axios
+      .get("http://127.0.0.1:8000/customer-groups?limit=:limit")
+      .then((response) => {
+        console.log(response)
+        this.customerGroups = response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+  
+}
+</script>
